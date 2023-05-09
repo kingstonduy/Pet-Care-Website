71a45df50bd88c06a81ab4dbdeacf9ae447305a2
kingstonduy <kingstonduy.duong@gmail.com>
2023-05-09 21:57:50 +0700

    80 percent account

--- petcareapp/src/MainApp.jsx
+++ petcareapp/src/MainApp.jsx
@@ -6,6 +6,7 @@ import Cat from './Components/Body/petInformation/Cat'
 import Dog from './Components/Body/petInformation/Dog'
 import Bird from './Components/Body/petInformation/Bird'
 import Hamster from './Components/Body/petInformation/Hamster'
+import Account from './Components/Body/account/Account';
 
 export default function MainApp(){
     return (
@@ -21,6 +22,7 @@ export default function MainApp(){
                     <Route path='/Dog' element={<Dog/>}/>
                     <Route path='/Bird' element={<Bird/>}/>
                     <Route path='/Hamster' element={<Hamster/>}/>
+                    <Route path='/Account' element={<Account/>}/>
                 </Routes>
             </BrowserRouter>
         </div>    
