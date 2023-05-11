7683eabffc4589bdfeb0b0cc7af1091934e5363a
nguyentrongnghia1702 <nguyentrongnghia1702@gmail.com>
2023-05-11 16:26:20 +0700

    fix

--- petcareapp/src/MainApp.jsx
+++ petcareapp/src/MainApp.jsx
@@ -9,14 +9,6 @@ import Hamster from './Components/Body/petInformation/Hamster'
 import Account from './Components/Body/account/Account';
 import ProductShop from './Components/Body/Products/ProductShop';
 import Login from './Components/Body/Login/Login';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Register from './Components/Body/Login/Register'
-=======
-import Footer from './Components/Footer/Footer';
-import About from './Components/Body/about/About';
->>>>>>> 175b307b59951f89b70ca221747cbb374f0293c8
-=======
 import Register from './Components/Body/Login/Register'
 import Footer from './Components/Footer/Footer';
 import About from './Components/Body/about/About';
@@ -37,19 +29,10 @@ export default function MainApp(){
                     <Route path='/Hamster' element={<Hamster/>}/>
                     <Route path='/Account' element={<Account/>}/>
                     <Route path='/Products' element={<ProductShop/>}/>
-<<<<<<< HEAD
-<<<<<<< HEAD
                     <Route path='/Login' element={<Login/>}/>
                     <Route path='/Register' element={<Register/>}/>
-=======
                     <Route path='/login' element={<Login/>}/>
-                    <Route path='About' element={<About/>}/>
->>>>>>> 175b307b59951f89b70ca221747cbb374f0293c8
-=======
-                    <Route path='/Login' element={<Login/>}/>
-                    <Route path='/Register' element={<Register/>}/>
-                    <Route path='About' element={<About/>}/>
->>>>>>> 025b20cd4fc0bfe65715ff394e9bfd27999a4a0c
+                    <Route path='/About' element={<About/>}/>
                 </Routes>
                 <Footer/>
 
