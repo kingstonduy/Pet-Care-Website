8fd9c91379a233bd04f76366891cbd97ab24c4fa
kingstonduy <kingstonduy.duong@gmail.com>
2023-05-15 14:00:35 +0700

    finish register

--- petcareapp/src/Components/apiClient/UserApi.js
+++ petcareapp/src/Components/apiClient/UserApi.js
@@ -7,3 +7,4 @@ import { apiClient } from "./Axios"
 export const checkLogin = (user) => apiClient.post('/login',user)
 export const getUsers = () => apiClient.get('/jpa/users')
 export const getUserByUsername = (username) => apiClient.get(`/users/user/${username}`)
+export const checkRegister = (user) => apiClient.post('/register',user)
